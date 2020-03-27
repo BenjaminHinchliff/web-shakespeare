5cad1befdfd7ad2271460eb071b9630814e39966
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-03-26 22:19:27 -0700

    moved some files

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
     filename: 'index.bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname),
     watchContentBase: true,
     compress: true,
     port: 9000,
