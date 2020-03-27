4a48637785a7c7f1bdd9c5e7f940f2c80ed8957f
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-03-26 20:56:36 -0700

    updated to add production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.bundle.js'
+    filename: 'index.bundle.js',
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     watchContentBase: true,
     compress: true,
-    port: 9000
-  }
+    port: 9000,
+  },
 };
